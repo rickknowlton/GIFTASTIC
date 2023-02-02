30174a998b0a9fba368e90bb9f9ec5d8dcf9019d
Rick Knowlton <richard.d.knowlton@gmail.com>
2023-02-01 22:56:03 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(async function () {
 
     //------------------------------------------//
     //                Gif Array                 //
@@ -7,7 +7,7 @@ $(document).ready(function () {
     // which we'll use to query the giphy API.  //  
     //------------------------------------------//
 
-    var topics = ["Tim and Eric", "Eric Andre", "Rick and Morty", "Superjail!", "Dr. Steve Brule", "Sealab 2021"];
+    const topics = ["Tim and Eric", "Eric Andre", "Rick and Morty", "Superjail!", "Dr. Steve Brule", "Sealab 2021"];
 
 
     //------------------------------------------//
@@ -19,19 +19,19 @@ $(document).ready(function () {
     // buttons.                                 //
     //------------------------------------------//
 
-    function populateButtons() {
-        $("#buttons").empty();
-        for (var g = 0; g < topics.length; g++) {
-            $("#buttons").append("<button class='gifBtn btn btn-sm btn-outline-secondary' dataGif='" + topics[g] + "'>" + topics[g] + "</button>");
-        }
+  function populateButtons() {
+    $("#buttons").empty();
+    for (let g = 0; g < topics.length; g++) {
+      $("#buttons").append(`<button class='gifBtn btn btn-sm btn-outline-secondary' dataGif='${topics[g]}'>${topics[g]}</button>`);
     }
+  }
 
-    $("#addBtn").on("click", function (event) {
-        event.preventDefault();
-        var newGif = $("#add").val();
-        topics.push(newGif);
-        populateButtons();
-    })
+  $("#addBtn").on("click", function (event) {
+    event.preventDefault();
+    const newGif = $("#add").val();
+    topics.push(newGif);
+    populateButtons();
+  });
 
     populateButtons();
 
@@ -48,37 +48,37 @@ $(document).ready(function () {
     //------------------------------------------//
 
 
-    $("#buttons").on("click", ".gifBtn", function () {
-        var gif = $(this).attr("dataGif");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC&limit=12";
-
-        $.ajax({
-                url: queryURL,
-                method: "GET"
-            })
-            .done(function (response) {
-                var results = response.data;
-                $("#gifs").empty();
-
-                for (var i = 0; i < results.length; i++) {
-                    var gifDiv = $("<div class='item col-md-4 col-lg-4'>");
-                    var gifImg = $("<img>");
-                    var p = $("<p class='rating'>").text("rated: " + results[i].rating);
-
-                    var dataState = gifImg.attr("data-state", "still");
-                    var dataStill = gifImg.attr("data-still", results[i].images.fixed_height_still.url);
-                    var dataAnimate = gifImg.attr("data-animate", results[i].images.fixed_height.url);
-
-                    gifImg.attr("src", results[i].images.fixed_height_still.url);
-                    gifDiv.prepend(p);
-                    gifDiv.prepend(gifImg);
-                    $("#gifs").prepend(gifDiv);
-                }
-            });
-
+    $("#buttons").on("click", ".gifBtn", async function () {
+        const gif = $(this).attr("dataGif");
+        const queryURL = `https://api.giphy.com/v1/gifs/search?q=${gif}&api_key=dc6zaTOxFJmzC&limit=12`;
+    
+        const response = await $.ajax({
+          url: queryURL,
+          method: "GET"
+        });
+    
+        const results = response.data;
+        $("#gifs").empty();
+    
+        for (let i = 0; i < results.length; i++) {
+          const gifDiv = $("<div class='item col-md-4 col-lg-4'>");
+          const gifImg = $("<img>");
+          const p = $("<p class='rating'>").text(`rated: ${results[i].rating}`);
+    
+          gifImg.attr({
+            "src": results[i].images.fixed_height_still.url,
+            "data-state": "still",
+            "data-still": results[i].images.fixed_height_still.url,
+            "data-animate": results[i].images.fixed_height.url
+          });
+    
+          gifDiv.prepend(p);
+          gifDiv.prepend(gifImg);
+          $("#gifs").prepend(gifDiv);
+        }
+    
         $(".portal").hide();
-
-    });
+      });
     
     //------------------------------------------//
     //           Animate Gif Function           //
@@ -91,16 +91,16 @@ $(document).ready(function () {
     //------------------------------------------//
 
     $("#gifs").on("click", ".item img", function () {
-        var state = $(this).attr("data-state");
-        var move = $(this).attr("data-animate");
-        var still = $(this).attr("data-still");
-
+        const state = $(this).attr("data-state");
+        const move = $(this).attr("data-animate");
+        const still = $(this).attr("data-still");
+    
         if (state === "still") {
-            $(this).attr("src", move);
-            $(this).attr("data-state", "animate");
+          $(this).attr("src", move);
+          $(this).attr("data-state", "animate");
         } else if (state === "animate") {
-            $(this).attr("src", still);
-            $(this).attr("data-state", "still");
+          $(this).attr("src", still);
+          $(this).attr("data-state", "still");
         }
     })
 });
